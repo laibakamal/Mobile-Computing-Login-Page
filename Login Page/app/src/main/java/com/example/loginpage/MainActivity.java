40691070997e7912735b03411d58b72ddfb7f8fb
LAIBA KAMAL <laibakamal003@gmail.com>
2022-05-19 23:34:46 +0500

    ...

--- Login Page/app/src/main/java/com/example/loginpage/MainActivity.java
+++ Login Page/app/src/main/java/com/example/loginpage/MainActivity.java
@@ -3,12 +3,50 @@ package com.example.loginpage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d("ALC 1", "onStart Activity Main");
+    }
+
+
+    @Override
+    protected void onResume()
+    {
+        super. onResume();
+        Log.d("ALC 1", " onResume Activity Main");
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super. onPause();
+        Log.d("ALC 1", " onPause Activity Main");
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super. onStop();
+        Log.d("ALC 1", " onStop Activity Main");
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super. onDestroy();
+        Log.d("ALC 1", " onDestroy Activity Main");
     }
 }
